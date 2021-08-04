76aa286a51c9dfe705b8e931783fce77320da940
Jared Gentry <jared.gentry@centauricorp.com>
2021-08-04 10:20:56 -0400

    Finish fundamentals section

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,27 +4,45 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
+  const inputEl = React.useRef(null)
+  const [userName, setUserName] = React.useState('')
+  // const [error, setError] = React.useState(null)
+
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
   // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+  function handleSubmit(event) {
+    event.preventDefault()
+    // event.target.elements.userName.value
+    // onSubmitUsername(inputEl.current.value)
+    onSubmitUsername(userName)
+  }
 
-  // 🐨 add the onSubmit handler to the <form> below
-
+  function handleChange(event) {
+    const value = event.target.value
+    setUserName(value.toLowerCase())
+    // const isValid = value === value.toLowerCase()
+    // setError(isValid ? null : 'Username must be lower case')
+  }
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        {/* <div role="alert" style={{color: 'red'}}>
+          {error}
+        </div> */}
+        <label htmlFor="userName">Username:</label>
+        <input
+          ref={inputEl}
+          id="userName"
+          name="userName"
+          type="text"
+          onChange={handleChange}
+          value={userName}
+        />
       </div>
+      {/* disabled={!!error} */}
       <button type="submit">Submit</button>
     </form>
   )
